9efb7b5cc55331fd5981aebd03438b60fe45ba12
Erick Alen Ricioli <ricioli@weg.net>
2023-08-24 13:38:42 -0300

    react configuration

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <h1>This is a test</h1>,
+  document.getElementById('root'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle[hash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //cifrao garante que termina com js no final
+        exclude: /node_modules/, // exclusão de node modules
+        use: 'babel-loader'
+      },
+    ],
+  },
+  devServer: {
+   port: 3000
+  }
+};
